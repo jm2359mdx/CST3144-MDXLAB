15f605bd05f5f4469163d4cbb561508e81e21772
jm2359 <jebinm98@gmail.com>
2025-10-04 12:18:48 +0100

    index and css structured for calculator

--- CalculatorJS/script.js
+++ CalculatorJS/script.js
@@ -1,14 +1,13 @@
-const x = document.getElementById('x');
-    const y = document.getElementById('y');
-    const result = document.getElementById('result');
+const x = document.getElementById("x");
+const y = document.getElementById("y");
+const result = document.getElementById("result");
 
-    function update() {
-      const xv = Number(x.value || 0);
-      const yv = Number(y.value || 0);
-      result.textContent = xv + yv;
-    }
+function update() {
+  const xv = Number(x.value || 0);
+  const yv = Number(y.value || 0);
+  result.textContent = xv + yv;
+}
 
-    x.addEventListener('input', update);
-    y.addEventListener('input', update);
-    update();
-  
+x.addEventListener("input", update);
+y.addEventListener("input", update);
+update();
